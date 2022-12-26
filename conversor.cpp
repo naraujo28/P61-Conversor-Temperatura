40289e9eda033c94eb0054e6bde3bfc84cb4b5c0
Nubia Araujo <naraujog@est.ups.edu.ec>
2022-12-26 18:35:02 -0500

    Final Convertidor a kelvin

--- conversor.cpp
+++ conversor.cpp
@@ -10,7 +10,9 @@ Conversor::Conversor(QWidget *parent)
     connect(ui->inCent, SIGNAL(valueChanged(int)),
             this, SLOT(cent2fahr(int)));
     connect(ui->inFahr, SIGNAL(valueChanged(int)),
-            this, SLOT (fahr2cent(int)));
+            this, SLOT(fahr2cent(int)));
+    connect(ui->inKel, SIGNAL(valueChanged(int)),
+            this, SLOT(kel2cent(int)));
 }
 
 Conversor::~Conversor()
@@ -22,7 +24,9 @@ void Conversor::cent2fahr(int grados)
 {
     if (ui->inCent->hasFocus()){
         int f = (grados * 9.0/5) + 32;
+        int k = grados + 273.15;
         ui->inFahr->setValue(f);
+        ui->inKel->setValue(k);
     }
 
 }
@@ -31,7 +35,19 @@ void Conversor::fahr2cent(int grados)
 {
     if (ui->inFahr->hasFocus()){
         int c = (grados - 32) * 5.0/9;
+        int k = (grados - 32) * 5.0/9 + 273.15;
         ui->inCent->setValue(c);
+        ui->inKel->setValue(k);
+    }
+}
+
+void Conversor::kel2cent(int grados)
+{
+    if (ui->inKel->hasFocus()){
+        int c = grados - 273.15;
+        int f = (grados - 273.15) * 9.0/5 + 32;
+        ui->inCent->setValue(c);
+        ui->inFahr->setValue(f);
     }
 }
 
--- conversor.h
+++ conversor.h
@@ -18,6 +18,7 @@ public:
 public slots:
     void cent2fahr(int grados);
     void fahr2cent(int grados);
+    void kel2cent(int grados);
 
 private:
     Ui::Conversor *ui;
